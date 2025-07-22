46dbab24892adc34551c9fa7afccef3492224d59
new-tonAA <164744587+new-tonAA@users.noreply.github.com>
2025-07-22 17:33:15 +0800

    Update config.py

--- Img_To_Img Flask/config.py
+++ Img_To_Img Flask/config.py
@@ -1,6 +1,6 @@
 class MysqlConfig(object):
     #sqlalchemy的配置参数
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:NewtonAA47@127.0.0.1:3306/db_pic_faiss'
+    SQLALCHEMY_DATABASE_URI = # connect to your own database
     #设置每次请求结束后会自动提交数据库的改动，一般设置手动保存
     SQLALCHEMY_COMMIT_ON_TEARDOWN=False
     #设置sqlachemy自动更新跟踪数据库
